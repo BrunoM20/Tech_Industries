3a95249eb7850f25219a45b35101da4271083e2d
Bruno Martins <103092367+BrunoM20@users.noreply.github.com>
2022-05-30 17:02:13 -0300

    Add files via upload

---
+++ Tech_Industries/JS.js
@@ -0,0 +1,34 @@
+// carrossel
+$('.carousel').slick({
+  dots: true,
+  infinite: true,
+  speed: 500,
+  fade: true,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  cssEase: 'linear'
+});
+
+// validar cadastro
+function validar(){
+  var senha = formuser.senha.value;
+  var rep_senha = formuser.rep_senha.value;
+    
+  if(senha == "" || senha.length <= 5){
+    alert('Preencha o campo senha com minimo 6 caracteres');
+    formuser.senha.focus();
+    return false;
+  }
+    
+  if(rep_senha == "" || rep_senha.length <= 5){
+    alert('Preencha o campo senha com minimo 6 caracteres');
+    formuser.rep_senha.focus();
+    return false;
+  }
+    
+  if (senha != rep_senha) {
+    alert('Senhas diferentes');
+    formuser.senha.focus();
+    return false;
+  }
+}  
